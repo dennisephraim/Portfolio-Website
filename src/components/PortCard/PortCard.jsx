a9555d3980f7b1c921e667c3a1fc36bae4a11417
dennisephraim <akainetteyephraim@gmail.com>
2024-09-17 21:58:15 -0400

    updates

--- src/components/PortCard/PortCard.jsx
+++ src/components/PortCard/PortCard.jsx
@@ -3,14 +3,16 @@ import './PortCard.css'
 
 const PortCard = (props) => {
     return (
-        <div className='port-card'>
-            <img src={props.img} className="card-img" alt="..."/>
-            <div className="port-card-body">
-                <p className="port-card-text">
-                    {props.text}
-                </p>
-            </div>
-        </div>  
+        <a className='htag' href={props.link}>
+            <div className='port-card'>
+                <img src={props.img} className="card-img" alt="..."/>
+                <div className="port-card-body">
+                    <p className="port-card-text">
+                        {props.text}
+                    </p>
+                </div>
+            </div>  
+        </a>
         )
     }
 export default PortCard;
--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Portfolio.css'
 import Port_Card from '../../components/PortCard/PortCard.jsx';
-import myimg from '../../assets/python.png'
+import netten_analytics from '../../assets/netten_analytics_hp.png'
+import perspective from '../../assets/perspective.png'
+import pong from '../../assets/pong_game_pic.png'
+import snake from '../../assets/snake_game_pic.png'
 
 const Portfolio = () => {
   return (
@@ -9,20 +12,24 @@ const Portfolio = () => {
       <h2 className='title'>My <span>Portfolio</span></h2>
       <div className='portfolio-container'>
         <Port_Card 
-          text="Some quick example text to build on the card title and make up the bulk of the card's content."
-          img={myimg}
+          text="Displays in a chart the revenue and net profit obtained by companies in the last five years"
+          img={netten_analytics}
+          link="https://github.com/dennisephraim/Netten-Analytics"
         />
         <Port_Card 
-          text="Some quick example text to build on the card title and make up the bulk of the card's content."
-          img={myimg}
+          text="Monitors a live price stream and triggers an alert when prices cross predefined boundaries"
+          img={perspective}
+          link="https://github.com/dennisephraim/Perspective"
         />
         <Port_Card 
-          text="Some quick example text to build on the card title and make up the bulk of the card's content."
-          img={myimg}
+          text="A simple two player ping pong game design with Turtle graphics"
+          img={pong}
+          link="https://github.com/dennisephraim/Python-Projects/tree/main/ping_pong"
         />
         <Port_Card 
-          text="Some quick example text to build on the card title and make up the bulk of the card's content."
-          img={myimg}
+          text="My version of snake built with python turtle graphics"
+          img={snake}
+          link="https://github.com/dennisephraim/Python-Projects/tree/main/snake_game_update"
         />
       </div>
     </div>
