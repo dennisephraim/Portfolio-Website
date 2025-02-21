c57558fc7ccec210b6352f1fd85eb54f7a67bae0
dennisephraim <akainetteyephraim@gmail.com>
2025-02-21 13:34:15 -0500

    added session cookies for users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,27 +9,64 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import admin from "firebase-admin";
+import cors from "cors";
+import express, {CookieOptions} from "express";
+import cookieParser from "cookie-parser";
 
 admin.initializeApp();
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
+const app = express();
+app.use(cookieParser());
+app.use(cors({
+  origin: true,
+  methods: ["GET", "POST", "OPTIONS"],
+  credentials: true,
+}));
 
-export const helloWorld = onRequest(async (request, response) => {
+app.post("/sessionCreation", async (req, res) => {
   try {
-    const authHeader = request.headers.authorization;
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      response.status(401).json({error: "Unauthorized: No token provided"});
-      return;
+    const token = req.body.idToken;
+    if (!token) {
+      return res.status(400).json({error: "No token provided"});
     }
-    const idToken = authHeader?.split("Bearer ")[1];
-    const decodedToken = await admin.auth().verifyIdToken(idToken);
-    const userId = decodedToken.uid;
-
-    response.status(200).send(
-      {message: "Hello from Firebase!", userId: userId}
+    const expiresIn = 5 * 24 * 60 * 60 * 1000;
+    const sessionCookie = await admin.auth().createSessionCookie(
+      token,
+      {expiresIn}
     );
+    const options = {
+      httpOnly: true,
+      secure: true,
+      maxAge: expiresIn,
+      sameSite: "none" as CookieOptions["sameSite"],
+    };
+    res.cookie("session", sessionCookie, options);
+    return res.json({message: "Session cookie set!"});
   } catch (error) {
-    response.status(403).send({error: "Forbidden: Invalid or expired token"});
-    return;
+    console.error(error);
+    return res.status(401).json(
+      {error: "Unauthorized"}
+    );
   }
 });
+
+app.get("/profile", async (req, res) => {
+  try {
+    const sessionCookie = req.cookies.session || "";
+    const decodedClaims = await admin.auth().verifySessionCookie(
+      sessionCookie, true
+    );
+
+    return res.json({
+      message: "Profile data here",
+      userId: decodedClaims.uid,
+      sign_in_provider: decodedClaims.firebase?.sign_in_provider,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({error: "Invalid or expired session cookie"});
+  }
+});
+
+export const sessionApp = onRequest(app);
