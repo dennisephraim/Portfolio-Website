6d50d1919173b2320ff09285c09db5bb5c582d42
dennisephraim <akainetteyephraim@gmail.com>
2025-02-23 01:06:33 -0500

    fixed token expiring

--- personal_website_project/src/context/context.tsx
+++ personal_website_project/src/context/context.tsx
@@ -56,7 +56,7 @@ export function SessionProvider({ children }: SessionProviderProps) {
             }
         });
 
-        unsubscribeAuthState()
+        () => unsubscribeAuthState()
     }, []);
 
     useEffect(() => {
