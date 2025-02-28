be4251a7c970af5034b84555ecc10b4fad73495b
dennisephraim <akainetteyephraim@gmail.com>
2025-02-28 15:02:51 -0500

    made profile improvements

--- personal_website_project/src/app/solo_leveling/page.tsx
+++ personal_website_project/src/app/solo_leveling/page.tsx
@@ -12,7 +12,7 @@ const temp_task = [
 ]
 
 export default function SoloLeveling() {
-    const {userId, role, loading, error} = useSession()
+    const {role, loading, error} = useSession()
     if (loading) return <p>Loading Session</p>
     if (error) return <p>Error: {error}</p>
 
