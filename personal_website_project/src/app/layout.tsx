46e39ecdba3b6fd65dd8da4ca4a89c9da1108798
dennisephraim <akainetteyephraim@gmail.com>
2025-03-18 21:41:29 -0400

    improving chat

--- personal_website_project/src/app/layout.tsx
+++ personal_website_project/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import "../components/navbar"
 import Navbar from "@/components/navbar";
 import { SessionProvider } from "@/context/context";
